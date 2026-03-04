8b4a817b19840f51f6de3776a1805a0c89a36d35
caio-unicamp <c288808@dac.unicamp.br>
2026-03-04 16:33:57 -0300

    Expandido para mostrar os dois primeiros displays

--- Lab00/Lab0_atividade2/hex_display.vhd
+++ Lab00/Lab0_atividade2/hex_display.vhd
@@ -6,7 +6,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity hex_display is
     Port (
         SW : in STD_LOGIC_VECTOR(9 downto 0);   -- 10 switches (usaremos apenas SW(3 downto 0))
-        HEX0 : out STD_LOGIC_VECTOR(6 downto 0) -- Display de 7 segmentos
+        HEX0 : out STD_LOGIC_VECTOR(6 downto 0); -- Primeiro Display de 7 segmentos
+		  HEX1: out STD_LOGIC_VECTOR(6 downto 0) -- Segundo Display de 7 segmentos
     );
 end hex_display;
 
@@ -17,7 +18,12 @@ begin
 			BIN => SW(3 downto 0),
 			HEX => HEX0
 		);
-
+	
+	display1: entity work.bin2hex
+		port map(
+			BIN => SW(7 downto 4),
+			HEX => HEX1
+		);
     -- TODO: Instanciar o componente bin2hex aqui
     -- Conecte SW(3 downto 0) à entrada BIN do bin2hex
     -- Conecte a saída HEX do bin2hex ao display HEX0
