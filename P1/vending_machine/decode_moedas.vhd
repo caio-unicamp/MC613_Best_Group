930d28af193e6125b61807d3dc3d9bc0a0dd5b3f
caio-unicamp <c288808@dac.unicamp.br>
2026-03-18 15:52:26 -0300

    Termina o decodificador de moedas englobando os casos de nenhum switch aceso ou mais de um

--- P1/vending_machine/decode_moedas.vhd
+++ P1/vending_machine/decode_moedas.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity decode_moedas is
     Port (
-        sw_moedas    : in  STD_LOGIC_VECTOR(5 downto 0); -- SW(9 downto 4)
+        sw_moedas    : in  STD_LOGIC_VECTOR(9 downto 4);
         valor_moeda  : out STD_LOGIC_VECTOR(10 downto 0);
         moeda_valida : out STD_LOGIC
     );
@@ -14,16 +14,16 @@ architecture Behavioral of decode_moedas is
 begin
     process(sw_moedas)
     begin
-        -- Assume que a moeda é válida por padrão, alterado caso caia no 'others'
+        -- Assume que a moeda é válida por padrão, muda caso caia no 'others'
         moeda_valida <= '1'; 
         
         case sw_moedas is
-            when "000001" => valor_moeda <= std_logic_vector(to_unsigned(5, 11));   -- SW(4): R$ 0,05
-            when "000010" => valor_moeda <= std_logic_vector(to_unsigned(10, 11));  -- SW(5): R$ 0,10
-            when "000100" => valor_moeda <= std_logic_vector(to_unsigned(25, 11));  -- SW(6): R$ 0,25
-            when "001000" => valor_moeda <= std_logic_vector(to_unsigned(50, 11));  -- SW(7): R$ 0,50
-            when "010000" => valor_moeda <= std_logic_vector(to_unsigned(100, 11)); -- SW(8): R$ 1,00
-            when "100000" => valor_moeda <= std_logic_vector(to_unsigned(200, 11)); -- SW(9): R$ 2,00
+            when "000001" => valor_moeda <= "00000000101"; -- 5 centavos
+            when "000010" => valor_moeda <= "00000001010"; -- 10 centavos
+            when "000100" => valor_moeda <= "00000011001"; -- 25 centavos
+            when "001000" => valor_moeda <= "00000110010"; -- 50 centavos
+            when "010000" => valor_moeda <= "00001100100"; -- 100 centavos (1 real)
+            when "100000" => valor_moeda <= "00011001000"; -- 200 centavos (2 reais)
             when others   => 
                 -- Nenhuma ou múltiplas chaves ativas
                 valor_moeda <= (others => '0');
