3938bd9c1856682ceb695d6608e62bc00a0569f2
caio-unicamp <c288808@dac.unicamp.br>
2026-03-24 17:43:38 -0300

    Não sei o q to fazendo espero q funcione

--- P1/vending_machine/maquina_estados.vhd
+++ P1/vending_machine/maquina_estados.vhd
@@ -1,26 +1,30 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+
 entity maquina_estados is
     Port (
         clk              : in  STD_LOGIC;
         rst              : in  STD_LOGIC;
+        -- Botões assíncronos
         avancar          : in  STD_LOGIC;
         cancelar         : in  STD_LOGIC;
+        -- Entradas do módulo do acumulador
         valor_acumulado  : in  STD_LOGIC_VECTOR(10 downto 0);
         saldo_suficiente : in  STD_LOGIC_VECTOR(10 downto 0);
         troco            : in  STD_LOGIC_VECTOR(10 downto 0);
-        done_timer             : in STD_LOGIC;    -- Recebido pelo contador de 1s quando ele encerra
+        -- Sinais do contador
+        done_timer       : in STD_LOGIC;    -- Recebido pelo contador de 1s quando ele encerra
+        reset_timer      : out STD_LOGIC;    -- Envia para o contador quando ele deve recomeçar a contar
         estado_atual     : out STD_LOGIC_VECTOR(2 downto 0);
-        clr_acumula      : out STD_LOGIC;
-        reset_timer      : out STD_LOGIC    -- Envia para o contador 
+        clr_acumula      : out STD_LOGIC
     );
 end maquina_estados;
 
 architecture Behavioral of maquina_estados is
 
     type estado_tipo is (escolher_produto, inserir_dinheiro, dispensar, devolver_normal, devolver_troco);
-    signal estado_atual, estado_proximo : estado_tipo;
+    estado_atual, estado_proximo : estado_tipo;
 
 begin
 
@@ -32,6 +36,16 @@ begin
         elsif rising_edge(clk) then
             estado_atual <= estado_proximo;
         end if;
+
+        when dispensar =>
+            if (troco) = '0' then
+                estado_proximo <= escolher_produto;
+            else
+                estado_proximo <= devolver_troco;
+            end if;
+
+        when devolver_troco =>
+        
     end process;
 
     -- Lógica de transição de estados
@@ -48,7 +62,7 @@ begin
 
             when inserir_dinheiro =>
                 if cancelar = '1' then
-                    if unsigned(valor_acumulado) = '0' then
+                    if (valor_acumulado) = '0' then
                         estado_proximo <= escolher_produto;
                     else
                         estado_proximo <= devolver_normal;
@@ -59,15 +73,9 @@ begin
                     estado_proximo <= inserir_dinheiro;
                 end if;
 
-            when dispensar =>
-                if unsigned(troco) = '0' then
-                    estado_proximo <= escolher_produto;
-                else
-                    estado_proximo <= devolver_troco;
-                end if;
-
-            when devolver =>    -- Precisa dividir em devolver normal e devolver troco
+            when devolver_normal =>    -- Precisa dividir em devolver normal e devolver troco
                 estado_proximo <= escolher_produto;
+
 
         end case;
     end process;
