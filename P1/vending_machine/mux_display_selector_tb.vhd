400c313944b3c992c631fe0e51885eeef489ae39
jpfrare <frarejp@gmail.com>
2026-03-31 20:19:09 -0300

    AGORA VAI PONTOCOM

--- P1/vending_machine/mux_display_selector_tb.vhd
+++ P1/vending_machine/mux_display_selector_tb.vhd
@@ -2,13 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity tb_mux_display_selector is
--- Testbench não possui portas
+entity mux_display_selector_tb is
 end entity;
 
 architecture behavior of mux_display_selector_tb is
 
-    -- Component Declaration para o UUT (Unit Under Test)
     component mux_display_selector
         port (
             estado          : in  std_logic_vector(2 downto 0);
@@ -19,7 +17,6 @@ architecture behavior of mux_display_selector_tb is
         );
     end component;
 
-    -- Sinais internos para conectar ao UUT
     signal s_estado          : std_logic_vector(2 downto 0) := "000";
     signal s_saldo_restante  : std_logic_vector(10 downto 0) := "00001100100"; -- 100
     signal s_valor_troco     : std_logic_vector(10 downto 0) := "00000011001"; -- 25
@@ -28,7 +25,6 @@ architecture behavior of mux_display_selector_tb is
 
 begin
 
-    -- Instância da Unidade Sob Teste (UUT)
     uut: mux_display_selector
         port map (
             estado          => s_estado,
@@ -38,37 +34,31 @@ begin
             saida_bin       => s_saida_bin
         );
 
-    -- Processo de estímulo
     stim_proc: process
     begin
-        -- Nota: Como é um circuito combinacional, não precisamos de clock,
-        -- apenas mudar as entradas e esperar um tempo para observar.
+        report "--- Iniciando Teste do Mux ---";
 
-        -- Teste Estado 000 (Saldo Restante)
+        -- Teste Estado 000
         s_estado <= "000";
-        wait for 20 ns;
+        wait for 10 ns;
+        report "Estado 000 (Saldo) -> Saida: " & integer'image(to_integer(unsigned(s_saida_bin)));
         
-        -- Teste Estado 001 (Saldo Restante)
-        s_estado <= "001";
-        wait for 20 ns;
-
-        -- Teste Estado 010 (Saldo Restante)
-        s_estado <= "010";
-        wait for 20 ns;
-
-        -- Teste Estado 011 (Valor Acumulado)
+        -- Teste Estado 011
         s_estado <= "011";
-        wait for 20 ns;
+        wait for 10 ns;
+        report "Estado 011 (Acumulado) -> Saida: " & integer'image(to_integer(unsigned(s_saida_bin)));
 
-        -- Teste Estado 100 (Valor Troco)
+        -- Teste Estado 100
         s_estado <= "100";
-        wait for 20 ns;
+        wait for 10 ns;
+        report "Estado 100 (Troco) -> Saida: " & integer'image(to_integer(unsigned(s_saida_bin)));
 
-        -- Teste Outros Estados (deve zerar a saída)
+        -- Teste Outros
         s_estado <= "111";
-        wait for 20 ns;
+        wait for 10 ns;
+        report "Estado Others (Zero) -> Saida: " & integer'image(to_integer(unsigned(s_saida_bin)));
 
-        -- Finaliza a simulação
+        report "--- Fim do Teste ---";
         wait;
     end process;
 
